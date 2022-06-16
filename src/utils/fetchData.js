718b098cbab186440fff6251915475590e43db6e
akashsingh <aakashsingh2702@gmail.com>
2022-06-17 02:24:33 +0530

    completed search component

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exercisesOptions = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "6dac457affmsh4bcdefd7ef6ef6cp14543cjsncb24e9018377",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
